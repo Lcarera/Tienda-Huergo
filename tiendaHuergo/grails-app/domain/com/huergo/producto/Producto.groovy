9e3fb95c97d9322ce7a5f93da1a91338162e6981
lcarera <lcarera04@gmail.com>
2022-10-07 17:01:57 -0300

    Agrego propiedad nombre a producto

--- tiendaHuergo/grails-app/domain/com/huergo/producto/Producto.groovy
+++ tiendaHuergo/grails-app/domain/com/huergo/producto/Producto.groovy
@@ -2,12 +2,14 @@ package com.huergo.producto
 
 class Producto {
 
+    String nombre
     Double precio
     Long cantidad
     TipoProducto tipo
 
     static hasMany = [talles:Talle]
     static constraints = {
+        nombre nullable:false
         precio nullable:false
         cantidad nullable:false
         tipo nullable:false

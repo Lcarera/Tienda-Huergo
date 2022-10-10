cf93ace5d00c4b1ecc7ba4100803ac31c1f69fa6
lcarera <lcarera04@gmail.com>
2022-10-10 10:56:01 -0300

    Mnado lista de productos al index de catalogo

--- tiendaHuergo/grails-app/controllers/com/huergo/catalogo/CatalogoController.groovy
+++ tiendaHuergo/grails-app/controllers/com/huergo/catalogo/CatalogoController.groovy
@@ -4,12 +4,18 @@ import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
 import grails.plugin.springsecurity.SpringSecurityUtils
 
+import com.huergo.producto.ProductoService
+
 import com.huergo.AccessRulesService
 @Secured(['ROLE_ADMIN','ROLE_USER'])
 class CatalogoController {
 
     AccessRulesService accessRulesService
+    ProductoService productoService
+    
     def index() {
+        def prods =  (productoService.getProductos())
+        [productos:productoService.getProductos() ]
     }
 
     def show() {

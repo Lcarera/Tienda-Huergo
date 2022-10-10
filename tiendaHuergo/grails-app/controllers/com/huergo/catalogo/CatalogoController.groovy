40813cec1be5166691773e35fde245cd4fe3d73f
lcarera <lcarera04@gmail.com>
2022-10-10 14:11:36 -0300

    Meotodos para agregar productos a carrito

--- tiendaHuergo/grails-app/controllers/com/huergo/catalogo/CatalogoController.groovy
+++ tiendaHuergo/grails-app/controllers/com/huergo/catalogo/CatalogoController.groovy
@@ -30,4 +30,21 @@ class CatalogoController {
     
     }
 
+    def ajaxAgregarCarrito(Long productoId){
+        def resultado = [:]
+		try {
+            def username = accessRulesService.currentUser
+			productoService.agregarACarrito(productoId, username)
+			resultado.error = false
+		}
+		catch(Exception e) {
+			log.error(e.message)
+			println e.stackTrace?.findAll{it.toString()?.with{contains(".groovy:") && ! toLowerCase().with{contains("transaction") || contains("springsecurity")}}}?.join("\n")
+			resultado.error = true
+		}
+		finally{
+			render resultado as JSON
+		}
+    }
+
 }
--- tiendaHuergo/grails-app/services/com/huergo/producto/ProductoService.groovy
+++ tiendaHuergo/grails-app/services/com/huergo/producto/ProductoService.groovy
@@ -1,13 +1,18 @@
 package com.huergo.producto
 
 import grails.transaction.Transactional
+import com.huergo.auth.User
+import com.huergo.compra.Carrito
+import com.huergo.compra.ProductoVenta
+import org.joda.time.LocalDateTime
+
 
 @Transactional
 class ProductoService {
 
     def sessionFactory
 
-    def getProductos(Long cuentaId){
+    def getProductos(){
         def query = """
             SELECT DISTINCT (NOMBRE)
             FROM PUBLIC.PRODUCTO
@@ -19,5 +24,22 @@ class ProductoService {
 
             return item
         }
+    }
+
+    def agregarACarrito(Long productoId, User user) {
+        def carrito = Carrito.findOrSaveWhere(usuario: user)
+        carrito.save(flush:true, failOnError:true)
+        def producto = Producto.get(productoId)
+        if(!producto){
+            Throw new Exception("No se encontro el producto con id: $productoId")
+        }
+        def producVenta = new ProductoVenta()
+        producVenta.producto = producto
+        producVenta.fechaAgregado = new LocalDateTime()
+        producVenta.carrito = carrito
+        producVenta.save(flush:true, failOnError:true)
+        carrito.addToProductos(producVenta)
+        carrito.save(flush:true, failOnError:true)
+        return
     }
 }

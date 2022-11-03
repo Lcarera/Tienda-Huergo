56d5650285d6a7ecefab944996e3c31177850b64
lcarera <lcarera04@gmail.com>
2022-11-03 16:50:29 -0300

    controller de checkout

---
+++ tiendaHuergo/grails-app/controllers/com/huergo/compra/CheckoutController.groovy
@@ -0,0 +1,26 @@
+package com.huergo.compra
+
+import grails.converters.JSON
+import grails.plugin.springsecurity.annotation.Secured
+import grails.plugin.springsecurity.SpringSecurityUtils
+
+@Secured(['ROLE_USER', 'ROLE_ADMIN'])
+class CheckoutController {
+
+    def index() {
+    
+    }
+
+    def show() {
+    
+    }
+
+    def save() {
+    
+    }
+
+    def delete() {
+    
+    }
+
+}
